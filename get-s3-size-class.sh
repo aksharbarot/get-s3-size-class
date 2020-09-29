6adaf268131cfa575f8d548422911bc9f2ff9b6b
aksharbarot <axar1990@gmail.com>
2020-09-29 21:45:41 +0530

    Update get-s3-size-class.sh

--- get-s3-size-class.sh
+++ get-s3-size-class.sh
@@ -6,6 +6,7 @@ case $sclass in
   GLACIER)            sclass="GlacierStorage" ;;
   DEEP_ARCHIVE)       sclass="DeepArchiveStorage" ;;
   STANDARD_IA)        sclass="StandardIAStorage" ;;
+  ONEZONE_IA)         sclass="OneZoneIAStorage" ;;
   *)                  sclass="StandardStorage" ;;
 esac
 size=$(aws --profile ABC cloudwatch get-metric-statistics --namespace AWS/S3 --start-time ${yesterday}T00:00:00 --end-time $(date +%F)T00:00:00 --period 86400 --metric-name BucketSizeBytes --dimensions Name=StorageType,Value=$sclass Name=BucketName,Value=$bucket --statistics Average --output text --query 'Datapoints[0].Average')
