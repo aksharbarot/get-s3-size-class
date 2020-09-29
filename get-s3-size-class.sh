c173f210e8c62755a4d1a597af428414298586d2
aksharbarot <axar1990@gmail.com>
2020-09-29 21:38:13 +0530

    Create get-s3-size-class.sh

---
+++ get-s3-size-class.sh
@@ -0,0 +1,17 @@
+yesterday=$(date -d @$((($(date +%s)-86400))) +%F)
+for bucket in `aws --profile ABC s3api list-buckets --query 'Buckets[*].Name' --output text`; do
+sclass=$(aws --profile ABC s3api list-objects --bucket $bucket --max-items=1 2> /dev/null | jq -r '.Contents[].StorageClass // "STANDARD"')
+#sclass=$(aws --profile ocpoc s3api list-objects --bucket $bucket --max-items=1 2> /dev/null | jq -r '.Contents[].StorageClass')
+case $sclass in
+  REDUCED_REDUNDANCY) sclass="ReducedRedundancyStorage" ;;
+  GLACIER)            sclass="GlacierStorage" ;;
+  DEEP_ARCHIVE)       sclass="DeepArchiveStorage" ;;
+  STANDARD_IA)        sclass="StandardIAStorage" ;;
+  *)                  sclass="StandardStorage" ;;
+esac
+size=$(aws --profile ABC cloudwatch get-metric-statistics --namespace AWS/S3 --start-time ${yesterday}T00:00:00 --end-time $(date +%F)T00:00:00 --period 86400 --metric-name BucketSizeBytes --dimensions Name=StorageType,Value=$sclass Name=BucketName,Value=$bucket --statistics Average --output text --query 'Datapoints[0].Average')
+if [ $size = "None" ]; then size=0; fi
+#printf $(echo $bucket)
+#printf $(echo $sclass)
+echo $bucket,$sclass,$size
+done
